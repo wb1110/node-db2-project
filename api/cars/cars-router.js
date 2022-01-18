5bbd5d9130725d77b82c3f263a612b968a4bd3bf
wb1110 <strengthcoachwb@gmail.com>
2022-01-18 13:06:03 -0600

    router setup

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,15 @@
 // DO YOUR MAGIC
 const Cars = require('./cars-model')
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  
+router.get('/', async (req, res, next) => {
+  const allCars = await Cars.getAll()
+  try {
+    res.json(allCars)
+  } catch (err) {
+    next(err)
+  }
 })
 
 
--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const server = express()
 
 server.use(express.json())
 
-server.use('api/cars', carsRouter)
+server.use('/api/cars', carsRouter)
 
 // DO YOUR MAGIC
 
